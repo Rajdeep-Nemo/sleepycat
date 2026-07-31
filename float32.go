ad702787714722b06bad787eb0347c6bbba86b53
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-07-31 19:54:43 +0530

    Proper comments are added

--- float32.go
+++ float32.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 )
 
+// Float32 reads an floating point number from the input source and returns as `float32`
 func Float32(opts ...option) (float32, error) {
 	return input(func(s string) (float32, error) {
 		v, err := strconv.ParseFloat(s, 32)
